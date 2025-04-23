380fc1f2fce2c489fa43ba015271643587dfae20
mervetanriverdi <mervetanriverdi.192@gmail.com>
2025-04-23 22:41:10 +0300

    Update main.py
    4. Gün: Kendine çarpma kontrolü eklendi

--- 2-gun/main.py
+++ 2-gun/main.py
@@ -1,24 +1,11 @@
 import pygame
 import sys
-
-pygame.init()
-
-ekran = pygame.display.set_mode((600, 400))
-pygame.display.set_caption("Yılan Oyunu - 2. Gün")
-saat = pygame.time.Clock()
-
-siyah = (0, 0, 0)
-yesil = (0, 255, 0)
-hucre = 20
-
-import pygame
-import sys
 import random
 
 pygame.init()
 
 ekran = pygame.display.set_mode((600, 400))
-pygame.display.set_caption("Yılan Oyunu - 3. Gün")
+pygame.display.set_caption("Yılan Oyunu - 4. Gün")
 saat = pygame.time.Clock()
 
 siyah = (0, 0, 0)
@@ -36,13 +23,13 @@ while True:
             pygame.quit()
             sys.exit()
         elif e.type == pygame.KEYDOWN:
-            if e.key == pygame.K_LEFT:
+            if e.key == pygame.K_LEFT and yon != "sag":
                 yon = "sol"
-            elif e.key == pygame.K_RIGHT:
+            elif e.key == pygame.K_RIGHT and yon != "sol":
                 yon = "sag"
-            elif e.key == pygame.K_UP:
+            elif e.key == pygame.K_UP and yon != "asagi":
                 yon = "yukari"
-            elif e.key == pygame.K_DOWN:
+            elif e.key == pygame.K_DOWN and yon != "yukari":
                 yon = "asagi"
 
     x, y = yilan[0]
@@ -55,19 +42,24 @@ while True:
     elif yon == "asagi":
         y += hucre
 
-    yilan.insert(0, (x, y))
+    yeni_bas = (x, y)
 
-    if yilan[0] == elma:
+    if yeni_bas in yilan:
+        print("Oyun Bitti! Yılan kendine çarptı.")
+        pygame.quit()
+        sys.exit()
+
+    yilan.insert(0, yeni_bas)
+
+    if yeni_bas == elma:
         elma = (random.randint(0, 29) * hucre, random.randint(0, 19) * hucre)
     else:
         yilan.pop()
 
     ekran.fill(siyah)
 
-    # Elmayı çiz
     pygame.draw.rect(ekran, (255, 0, 0), (elma[0], elma[1], hucre, hucre))
 
-    # Yılanı çiz
     for parca in yilan:
         pygame.draw.rect(ekran, yesil, (parca[0], parca[1], hucre, hucre))
 
