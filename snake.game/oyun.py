86a20a793334421cfebbfe1f2fe5cee17b4b8c30
mervetanriverdi <mervetanriverdi.192@gmail.com>
2025-05-26 21:51:51 +0300

    Update oyun.py
    skor artık devamlı görünüyor

--- snake.game/oyun.py
+++ snake.game/oyun.py
@@ -23,24 +23,29 @@ class Oyun:
 
     def ciz(self):
         self.ekran.fill((0, 0, 0))
-        for parca in self.yilan_pos:
-            pygame.draw.rect(self.ekran, (0, 255, 0), pygame.Rect(parca[0], parca[1], 10, 10))
+        for segment in self.yilan_pos:
+            pygame.draw.rect(self.ekran, (0, 255, 0), pygame.Rect(segment[0], segment[1], 10, 10))
         pygame.draw.rect(self.ekran, (255, 0, 0), pygame.Rect(self.elma[0], self.elma[1], 10, 10))
+
+        # Skoru sağ üst köşede göster
+        skor_yazi = self.font.render(f"Skor: {self.skor}", True, (255, 255, 255))
+        self.ekran.blit(skor_yazi, (self.genislik - skor_yazi.get_width() - 10, 10))
+
         pygame.display.flip()
 
     def hareket_et(self):
-        bas = self.yilan_pos[0][:]
+        yeni_bas = self.yilan_pos[0][:]
         if self.yon == 'UP':
-            bas[1] -= 10
+            yeni_bas[1] -= 10
         elif self.yon == 'DOWN':
-            bas[1] += 10
+            yeni_bas[1] += 10
         elif self.yon == 'LEFT':
-            bas[0] -= 10
+            yeni_bas[0] -= 10
         elif self.yon == 'RIGHT':
-            bas[0] += 10
-        self.yilan_pos.insert(0, bas)
+            yeni_bas[0] += 10
+        self.yilan_pos.insert(0, yeni_bas)
 
-        if bas == self.elma:
+        if yeni_bas == self.elma:
             self.skor += 1
             self.elma = self.yeni_elma()
         else:
@@ -48,9 +53,11 @@ class Oyun:
 
     def carpisma_kontrol(self):
         bas = self.yilan_pos[0]
-        return (bas in self.yilan_pos[1:] or
-                bas[0] < 0 or bas[0] >= self.genislik or
-                bas[1] < 0 or bas[1] >= self.yukseklik)
+        return (
+            bas in self.yilan_pos[1:] or
+            bas[0] < 0 or bas[0] >= self.genislik or
+            bas[1] < 0 or bas[1] >= self.yukseklik
+        )
 
     def tus_kontrol(self):
         for etkinlik in pygame.event.get():
@@ -73,6 +80,8 @@ class Oyun:
         self.ekran.blit(yazi, (self.genislik // 2 - yazi.get_width() // 2, self.yukseklik // 2 - 20))
         pygame.display.flip()
         pygame.time.wait(3000)
+        pygame.quit()
+        sys.exit()
 
     def calistir(self):
         oyun_devam = True
@@ -84,6 +93,5 @@ class Oyun:
             self.ciz()
             self.saat.tick(self.hiz)
         self.oyun_bitti_ekrani()
-        pygame.quit()
-        return self.skor
+
 
